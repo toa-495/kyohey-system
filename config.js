fbc66af91ec3b9b512c84c858d60caf87f0acf1e
toa-495 <k.oda@yumephoto.com>
2026-06-01 11:01:01 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   // GASをデプロイしたあと、/exec のURLに差し替えてください。
-  GAS_BASE_URL: 'https://script.google.com/macros/s/AKfycbzNoMUOQYDgJ51ajW85XPWzOSjc7IsdtGNBTZbgFBy4lZDulEOsFfsBrcr5Nh7MTe7VpQ/exec',
+  GAS_BASE_URL: 'https://script.google.com/macros/s/AKfycbycKMYD3bAL8ctpRvrwvPTNlglxe6lZoRyreqOajTl7GJ3mdNeLKeb0oikC3l-s7I29eQ/exec',
 };
